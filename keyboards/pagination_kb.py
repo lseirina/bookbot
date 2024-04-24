40deb7316790b3bd3bd532966337a831aca4fde3
root <lseirina87@gmail.com>
2024-04-24 14:26:14 +0300

    Added create bookmarks keyboard

--- keyboards/pagination_kb.py
+++ keyboards/pagination_kb.py
@@ -7,7 +7,7 @@ from lexicon.lexicon import LEXICON
 def create_pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
     kb_builder = InlineKeyboardBuilder()
     kb_builder.row([InlineKeyboardButton(
-        text=LEXICON.get(button, button),
+        text=LEXICON.get(button, button),  # text=LEXICON[button] if button in LEXICON else button
         callback_data=button
     ) for button in buttons])
 
