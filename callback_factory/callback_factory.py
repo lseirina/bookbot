26e1e24079290c7d508cddefd83c5f9f76e62060
root <lseirina87@gmail.com>
2024-04-29 21:00:44 +0300

    Added callback factory  filter

---
+++ callback_factory/callback_factory.py
@@ -0,0 +1,67 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    CallbackQuery,
+    Message,
+)
+
+BOT_TOKEN = '6883498485:AAGtOZFurG3T-H2oDNwhQcUqeUzlMfqcJHE'
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+class GoodsCallbackDataFactory(CallbackData, prefix='goods'):
+    id_category: int
+    id_subcategory: int
+    id_item: int
+
+
+button_1 = InlineKeyboardButton(
+    text='1 Category',
+    callback_data=GoodsCallbackDataFactory(
+        id_category=1,
+        id_subcategory=0,
+        id_item=0
+    ).pack()
+)
+button_2 = InlineKeyboardButton(
+    text="2 category",
+    callback_data=GoodsCallbackDataFactory(
+        id_category=2,
+        id_subcategory=0,
+        id_item=0
+    ).pack()
+)
+markup = InlineKeyboardMarkup(
+    inline_keyboard=[[button_1], [button_2]]
+    )
+
+
+@dp.message(CommandStart)
+async def process_start_command(message: Message):
+    await message.answer(
+        text='This is inlineKeyboard',
+        reply_markup=markup
+    )
+
+
+
+@dp.callback_query(GoodsCallbackDataFactory.filter())
+async def process_category_press(callback: CallbackQuery,
+                                 callback_data: GoodsCallbackDataFactory
+                                 ):
+    await callback.message.answer(text=callback_data.pack())
+    await callback.answer()
+
+
+@dp.callback_query()
+async def procces_button_press(callback: CallbackQuery):
+    await callback.model_dump_json(indent=4, exclude_none=True)
+    await callback.answer()
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
