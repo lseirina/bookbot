630d9eb53521e90c5c1b95c29db34de4cf13893b
root <lseirina87@gmail.com>
2024-05-08 11:54:04 +0300

    Added test_bookmark_command

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,5 +1,5 @@
 from unittest import IsolatedAsyncioTestCase
-from unittest.mock import MagicMock, AsyncMock
+from unittest.mock import MagicMock, AsyncMock, patch
 
 from lexicon.lexicon import LEXICON
 from aiogram.types import Message
@@ -14,3 +14,13 @@ class UserHandlerTest(IsolatedAsyncioTestCase):
 
         await router.procces_start_command(message)
         message.answer.assert_called_with(LEXICON['/start'])
+
+    async def test_bookmark_command(self):
+        message = MagicMock()
+        message.from_user.id = 123
+        message.text = '/bookmarks'
+        message.answer = AsyncMock()
+        user_db = {123: {'bookmarks': [1, 2, 3]}}
+        with patch(database.database.user_db, user_db):
+            await router.process_bookmark_command(message)
+            message.answer.assert_called_with('This the list of your bookmarks:\n1\n2\n3')
